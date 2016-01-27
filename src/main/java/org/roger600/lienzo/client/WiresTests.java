f5171fd2eb89fc3674d9abcffa1a59e3c41ddb0b
Douglas Campos <qmx@qmx.me>
2016-01-27 02:19:33 -0200

    checkpoint

--- src/main/java/org/roger600/lienzo/client/WiresTests.java
+++ src/main/java/org/roger600/lienzo/client/WiresTests.java
@@ -1,12 +1,37 @@
 package org.roger600.lienzo.client;
 
 import com.ait.lienzo.client.core.event.NodeMouseClickEvent;
+import com.ait.lienzo.client.core.shape.Circle;
+import com.ait.lienzo.client.core.shape.Group;
 import com.ait.lienzo.client.core.shape.Layer;
 import com.ait.lienzo.client.core.shape.MultiPath;
+import com.ait.lienzo.client.core.shape.SVGPath;
+import com.ait.lienzo.client.core.shape.Star;
+import com.ait.lienzo.client.core.shape.wires.IConnectionAcceptor;
+import com.ait.lienzo.client.core.shape.wires.IContainmentAcceptor;
+import com.ait.lienzo.client.core.shape.wires.IControlHandle;
 import com.ait.lienzo.client.core.shape.wires.IControlHandleList;
+import com.ait.lienzo.client.core.shape.wires.WiresConnection;
+import com.ait.lienzo.client.core.shape.wires.WiresContainer;
+import com.ait.lienzo.client.core.shape.wires.WiresLayer;
+import com.ait.lienzo.client.core.shape.wires.WiresMagnet;
+import com.ait.lienzo.client.core.shape.wires.WiresManager;
+import com.ait.lienzo.client.core.shape.wires.WiresShape;
+import com.ait.lienzo.client.core.types.BoundingBox;
+import com.ait.lienzo.client.core.types.PathPartEntryJSO;
+import com.ait.lienzo.client.core.types.PathPartList;
+import com.ait.lienzo.client.core.types.Point2D;
+import com.ait.lienzo.client.core.types.Point2DArray;
+import com.ait.lienzo.client.core.util.Geometry;
+import com.ait.lienzo.shared.core.types.ColorName;
 import com.ait.lienzo.shared.core.types.Direction;
+import com.ait.tooling.nativetools.client.collection.NFastArrayList;
+import com.ait.tooling.nativetools.client.collection.NFastDoubleArrayJSO;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.user.client.ui.FlowPanel;
+
+import java.util.HashSet;
+import java.util.Set;
 
 public class WiresTests extends FlowPanel {
 
@@ -20,43 +45,16 @@ public class WiresTests extends FlowPanel {
 
     public void testWires() {
 
-        final double startX = 200;
-        final double startY = 200;
-        final double radius = 50;
-        final double w = 100;
-        final double h = 100;
-
-        // Blue start event.
-        MultiPath box1 = new MultiPath().rect(0, 0, w, h).setFillColor("#000000");
-        box1.setX(startX);
-        box1.setY(startY);
-
-        MultiPath box2 = new MultiPath().rect(0, 0, w, h).setFillColor("#000000");
-        box2.setX(startX * 2);
-        box2.setY(startY * 2);
-
-        layer.add(box2);
-
-        Toolbox toolbox = new Toolbox(box2, Direction.SOUTH_EAST);
-        new ToolboxButton(toolbox, "icons/activity/task.png", new ToolboxButtonClickHandler() {
-            @Override
-            public void onClick(ToolboxButton button, NodeMouseClickEvent event) {
-                GWT.log("task clicked");
-            }
-        });
-        new ToolboxButton(toolbox, "icons/activity/subprocess.png", new ToolboxButtonClickHandler() {
-            @Override
-            public void onClick(ToolboxButton button, NodeMouseClickEvent event) {
-                GWT.log("subprocess clicked");
-            }
-        });
-        new ToolboxButton(toolbox, "icons/connector/messageflow.png", new ToolboxButtonClickHandler() {
-            @Override
-            public void onClick(ToolboxButton button, NodeMouseClickEvent event) {
-                GWT.log("messageflow clicked");
-            }
-        });
-
-        boolean init = false;
+        MultiPath rect = new MultiPath().rect(0, 0, 100, 100);
+        layer.add(rect);
+        Point2DArray listOfLines = new Point2DArray();
+        listOfLines.push(50, 50);
+        listOfLines.push(150, 50);
+        Set<Point2D>[] intersections = new Set[1];
+        Geometry.getCardinalIntersects(rect.getPathPartList(), listOfLines, intersections);
+        GWT.log(intersections.toString());
     }
+
+
+
 }
